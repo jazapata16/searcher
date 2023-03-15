e6bc50148f5859cd8cc4188e1e7a97993e4cb6ce
jazapata16 <107829063+jazapata16@users.noreply.github.com>
2023-03-15 12:11:51 -0500

    Correccion(mal subido)

--- assets/js/searcher.js
+++ assets/js/searcher.js
@@ -7,6 +7,64 @@
         const boton = document.querySelector("#filter");
         let element_2=document.getElementById("text");
 
+        fetch(myURL1)
+            .then(response=> response.json())
+            .then(products=>{
+        let name=document.getElementById("name1");
+        name.innerHTML=products[0]['name'] ;
+        let type=document.getElementById("type1");
+        type.innerHTML=products[0]['type'];                      
+        let price=document.getElementById("price1");
+        price.innerHTML=products[0]['price'];
+        document.getElementById("img1").src=products[0]['src'];
+
+        let name2=document.getElementById("name2");
+        name2.innerHTML=products[1]['name'] ;
+        let type2=document.getElementById("type2");
+        type2.innerHTML=products[1]['type'];                      
+        let price2=document.getElementById("price2");
+        price2.innerHTML=products[1]['price'];
+        document.getElementById("img2").src=products[1]['src'];
+        
+
+        let name3=document.getElementById("name3");
+        name3.innerHTML=products[2]['name'] ;
+        let type3=document.getElementById("type3");
+        type3.innerHTML=products[2]['type'];                      
+        let price3=document.getElementById("price3");
+        price3.innerHTML=products[2]['price'];
+        document.getElementById("img3").src=products[2]['src'];
+
+        let name4=document.getElementById("name4");
+        name4.innerHTML=products[3]['name'] ;
+        let type4=document.getElementById("type4");
+        type4.innerHTML=products[3]['type'];                      
+        let price4=document.getElementById("price4");
+        price4.innerHTML=products[3]['price'];
+        document.getElementById("img4").src=products[3]['src'];
+
+        let name5=document.getElementById("name5");
+        name5.innerHTML=products[4]['name'] ;
+        let type5=document.getElementById("type5");
+        type5.innerHTML=products[4]['type'];                      
+        let price5=document.getElementById("price5");
+        price5.innerHTML=products[4]['price'];
+        document.getElementById("img5").src=products[4]['src'];
+
+        let name6=document.getElementById("name6");
+        name6.innerHTML=products[5]['name'] ;
+        let type6=document.getElementById("type6");
+        type6.innerHTML=products[5]['type'];                      
+        let price6=document.getElementById("price6");
+        price6.innerHTML=products[5]['price'];
+        document.getElementById("img6").src=products[5]['src'];
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+
         const cuandoSeHaceClick = function (evento) {
             // Recuerda, this es el elemento
             console.log("El texto que tiene es: ", this.innerText);
