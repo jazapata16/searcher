3270478ff5a5d66dd5c8c000d525d7eafb63caad
jazapata16 <107829063+jazapata16@users.noreply.github.com>
2023-03-15 08:17:29 -0500

    Actualizacion

--- assets/js/searcher.js
+++ assets/js/searcher.js
@@ -1,15 +1,73 @@
 
-    let loadProducts=()=>{
-        fetch('https://raw.githubusercontent.com/Bootcamp-Espol/FSD02/main/S03D03/clase/recursos/products.json')
-        .then(response=> response.json())
-        .then(products=>{
+    
+    let URL1="https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.json"
+    let URL2="https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.xml"
 
-        })
-        fetch('https://raw.githubusercontent.com/Bootcamp-Espol/FSD02/main/S03D03/clase/recursos/products.xml')
+    let loadProducts=(myURL1,myURL2)=>{
+        const boton = document.querySelector("#filter");
+        let element_2=document.getElementById("text");
+
+        const cuandoSeHaceClick = function (evento) {
+            // Recuerda, this es el elemento
+            console.log("El texto que tiene es: ", this.innerText);
+    
+        }
+        
+        boton.addEventListener("click", function(evento){
+
+            let valores= element_2.value
+            console.log(valores);
+
+            fetch(myURL1)
+            .then(response=> response.json())
+            .then(products=>{
+                console.log(products);
+                
+
+                for (i = 0; i < products.length; i++){
+
+                    console.log(products[i]["type"]);
+                    if (products[i]["type"] === valores){
+                        console.log(products[i]);
+                        
+                        let name=document.getElementById("name"+(i+1));
+                        name.innerHTML=products[i]['name'] ;
+
+                        let type=document.getElementById("type"+(i+1));
+                        type.innerHTML=products[i]['type'];
+                        
+                        let price=document.getElementById("price"+(i+1));
+                        price.innerHTML=products[i]["price"];
+
+                        console.log(products[i]["src"]);
+
+                        document.getElementById("img"+(i+1)).src=products[i]["src"];
+
+
+                    }
+                }  
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        });
+
+        boton.addEventListener("click", cuandoSeHaceClick);
+
+    
+
+        fetch(myURL2)
         .then(response=> response.text())
         .then(result=>{
-            
+
         })
+        .catch(error => {
+            console.log(error);
+        });
+        
+
+
+    
     }
 
-loadProducts();
+loadProducts(URL1,URL2);
