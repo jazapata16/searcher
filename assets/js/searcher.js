84fe4ca6be8b31799d43c1a08d72a07a4307ffd1
jazapata16 <107829063+jazapata16@users.noreply.github.com>
2023-03-15 09:00:24 -0500

    Actualizacion

--- assets/js/searcher.js
+++ assets/js/searcher.js
@@ -17,14 +17,51 @@
 
             let valores= element_2.value
             console.log(valores);
+            let indice=1;
 
             fetch(myURL1)
             .then(response=> response.json())
             .then(products=>{
-                console.log(products);
-                
-
                 for (i = 0; i < products.length; i++){
+
+                    let name=document.getElementById("name"+(i+1));
+                        name.innerHTML="" ;
+
+                        let type=document.getElementById("type"+(i+1));
+                        type.innerHTML="";
+                        
+                        let price=document.getElementById("price"+(i+1));
+                        price.innerHTML="";
+
+                        document.getElementById("img"+(i+1)).src="";
+
+                    console.log(products[i]["type"]);
+                    if (products[i]["type"] === valores){
+                        console.log(products[i]);
+                        
+                        let name=document.getElementById("name"+(indice));
+                        name.innerHTML=products[i]['name'] ;
+
+                        let type=document.getElementById("type"+(indice));
+                        type.innerHTML=products[i]['type'];
+                        
+                        let price=document.getElementById("price"+(indice));
+                        price.innerHTML=products[i]["price"];
+
+                        document.getElementById("img"+(indice)).src=products[i]["src"];
+                        indice+=1;
+                    }
+                }  
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+
+            fetch(myURL2)
+            .then(response=> response.text())
+            .then(result=>{
+                for (i = 0; i < result.length; i++){
 
                     console.log(products[i]["type"]);
                     if (products[i]["type"] === valores){
@@ -39,34 +76,20 @@
                         let price=document.getElementById("price"+(i+1));
                         price.innerHTML=products[i]["price"];
 
-                        console.log(products[i]["src"]);
-
                         document.getElementById("img"+(i+1)).src=products[i]["src"];
-
-
                     }
                 }  
+
+
             })
             .catch(error => {
                 console.log(error);
             });
+
         });
+
 
         boton.addEventListener("click", cuandoSeHaceClick);
-
-    
-
-        fetch(myURL2)
-        .then(response=> response.text())
-        .then(result=>{
-
-        })
-        .catch(error => {
-            console.log(error);
-        });
-        
-
-
     
     }
 
